7c2f2ab97d8430774585a831f6da63493632c6be
persona <illuminati@hunddansen.se>
2015-10-26 02:33:55 +0100

    Added sphere and shape types - started working on lines

--- hello_seymore/out/hello_seymore/core.cljs
+++ hello_seymore/out/hello_seymore/core.cljs
@@ -3,15 +3,20 @@
             [goog.events.EventType :as event-type]))
 
 (def state (atom 
-            [{:type :rectangle :x 300 :y 300 :h 400 :w 400 :vx 5 :vy 2 :color "#ccc"}
-             {:type :rectangle :x 400 :y 400 :h 200 :w 200 :vx 5 :vy 2 :color "#aaa"}
-             {:type :rectangle :x 450 :y 450 :h 100 :w 100 :vx 5 :vy 2 :color "#333"}
-             {:type :rectangle :x 300 :y 300 :h 100 :w 100 :vx 5 :vy 2 :color "#aaa"}
-             {:type :rectangle :x 600 :y 600 :h 100 :w 100 :vx 5 :vy 2 :color "#aaa"}
-             {:type :rectangle :x 600 :y 300 :h 100 :w 100 :vx 5 :vy 2 :color "#aaa"}
-             {:type :rectangle :x 300 :y 600 :h 100 :w 100 :vx 5 :vy 2 :color "#aaa"}
+            [
+             ;; {:type :rectangle :x 300 :y 300 :h 400 :w 400 :dx 5 :dy 2 :color "#ccc"}
+             ;; {:type :rectangle :x 400 :y 400 :h 200 :w 200 :dx 5 :dy 2 :color "#aaa"}
+             ;; {:type :rectangle :x 450 :y 450 :h 100 :w 100 :dx 5 :dy 2 :color "#333"}
+             ;; {:type :rectangle :x 300 :y 300 :h 100 :w 100 :dx 5 :dy 2 :color "#aaa"}
+             ;; {:type :rectangle :x 600 :y 600 :h 100 :w 100 :dx 5 :dy 2 :color "#aaa"}
+             ;; {:type :rectangle :x 600 :y 300 :h 100 :w 100 :dx 5 :dy 2 :color "#aaa"}
+             ;;{:type :rectangle :x 300 :y 600 :h 100 :w 100 :dx 5 :dy 2 :color "#aaa"}
+             {:type :circle :radius 50  :vx 2 :vy 2 :color "#aaa"}
+             {:type :line :start {:x 500 :y 500} :end {:x 700 :y 500} :vx 0 :vy 2 :color "#fff"}
              ]
             ))
+
+(def pi-times-2 (* Math/PI 2))
 
 (def canvas-dom (.getElementById js/document "c1"))
 
@@ -29,12 +34,20 @@
 
 (set! (.-height canvas-dom) inner-height)
 
+(defn draw-circle [x y radius start-angle end-angle]
+  (.beginPath context)
+  (.arc context x y radius start-angle end-angle)
+  (.stroke context))
+
 (defn draw! [state]
   (do
     (set! (. context -fillStyle) (:color (first state)))
-    (doseq [rect state]
-      (set! (. context -fillStyle) (:color rect))
-      (.fillRect context (:x rect) (:y rect) (:w rect) (:h rect)))
+    (doseq [shape state]
+      (set! (. context -fillStyle) (:color shape))      
+      (condp = (:type shape)
+        :rectangle (.fillRect context (:x shape) (:y shape) (:w shape) (:h shape))
+        :circle  (draw-circle (:x shape) (:y shape) (:radius shape) 0 pi-times-2))
+      )
     ))
 
 (defn updater [state]
@@ -62,12 +75,33 @@
 (defn direction-change-y [state]
   (map (fn [shape] (update shape :vy #(- %))) state))
 
+(defn line-distance [start end]
+  (let [xs (- (:x end) (:x start))
+        ys (- (:y end) (:y start))]
+    (Math/sqrt (+ (* xs xs) (* ys ys)))))
+
+(defn update-line-delta! [line state]
+  (if (or (>= (:y (:end line)) (.-innerHeight js/window)) (<= (:end line 0)))
+    (swap! state ????)))
+
+(defn update-rectangle-delta! [rect state]
+    (if (or (>= (+ (:x rect) (:w rect)) (.-innerWidth js/window)) (<= (:x rect) 0))
+      (swap! state direction-change-x))
+    (if (or (>= (+ (:y rect) (:h rect)) (.-innerHeight js/window)) (<= (:y rect) 0))
+      (swap! state direction-change-y)))
+
+(defn update-deltas! [state]
+  (doseq [shape state]
+    (condp = (:type shape)
+      :rectangle (update-rectangle-delta! shape state)
+      :line (update-line-delta! shape state))))
+
 (defn render! [state]  
   (do
     (clear!)     
-    (if (or (>= (+ (:x (first @state)) (:w (first @state))) inner-width) (<= (:x (first @state)) 0))
+    (if (or (>= (+ (:x (first @state)) (:w (first @state))) (.-innerWidth js/window)) (<= (:x (first @state)) 0))
       (swap! state direction-change-x))
-    (if (or (>= (+ (:y (first @state)) (:h (first @state))) inner-height) (<= (:y (first @state)) 0))
+    (if (or (>= (+ (:y (first @state)) (:h (first @state))) (.-innerHeight js/window)) (<= (:y (first @state)) 0))
       (swap! state direction-change-y))
     (swap! state updater)
     (draw! @state))
@@ -87,8 +121,9 @@
     ;; (.log js/console (str "offsetX" e.offsetX))
     ;; (.log js/console (str "offsetY" e.offsetY))
     ;; (.log js/console (str "event X" event.x))
-    ;; (.log js/console (str "event Y" event.y))
+    ;;(.log js/console (str "event Y" event.y))
     (render! state)
+    ;;(set! (. context -fillStyle) "#fff")          
     ;;(set! (. context -fillStyle) "#fff")
     ;;(.fillRect context (- event.x 15) (- event.y 15) 10 10)
     ))
